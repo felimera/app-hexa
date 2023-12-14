50bf91854900a76889eb437e39489fd7b41b0e15
andres.mera <feli041089@gmail.com>
2023-12-14 14:06:53 -0500

    Creacion del proyecto Maven.

---
+++ src/main/java/com/hexagonal/apphexa/AppHexaApplication.java
@@ -0,0 +1,13 @@
+package com.hexagonal.apphexa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppHexaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppHexaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hexagonal/apphexa/AppHexaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hexagonal.apphexa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppHexaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
