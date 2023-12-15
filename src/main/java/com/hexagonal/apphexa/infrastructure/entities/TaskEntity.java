a4056c7189bf92ea6aa2b6c14086fa4fc1224048
andres.mera <feli041089@gmail.com>
2023-12-15 07:30:09 -0500

    dev : Creacion y definicion de de los atributos de la entidad.

---
+++ src/main/java/com/hexagonal/apphexa/infrastructure/entities/TaskEntity.java
@@ -0,0 +1,32 @@
+package com.hexagonal.apphexa.infrastructure.entities;
+
+import com.hexagonal.apphexa.domain.models.Task;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class TaskEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private LocalDateTime creatioDate;
+    private Boolean completed;
+
+    public static TaskEntity fromDomainModel(Task task) {
+        return new TaskEntity(task.getId(), task.getTitle(), task.getDescription(), task.getCreationDate(), task.getCompleted());
+    }
+}
